023a21b32699c1a6d1fcf686901ac17be111d4ad
LapisMYT <nikitagavrilin005@gmail.com>
2023-08-31 10:58:30 +0300

    Start project structure

---
+++ bedrockproto/server.py
@@ -0,0 +1,11 @@
+from pieraknet import Server
+from pieraknet.packets.game_packet import GamePacket
+from pieraknet.connection import Connection
+import logging
+import os
+import time
+import random
+
+class MCBEServer:
+    def __init__(self, hostname, port):
+        pass
