08c1366c6ed1b7e4356d7db48deb3d8b8c519b02
Jeremiah Saunders <jeremiahsaunders5@gmail.com>
2024-06-03 17:18:22 -0500

    Move JWT data to __init__ and remove extra TODO

--- piebedrock/packets/server_client_handshake.py
+++ piebedrock/packets/server_client_handshake.py
@@ -7,7 +7,9 @@ class ServerClientHandshakePacket(BedrockPacket):
     PACKET_ID = 0x03
     PACKET_TYPE = "server_client_handshake"
 
-    jwt_data: str = None
+    def __init__(self, jwt_data: Union[str, None] = None):
+        super().__init__()
+        self.jwt_data = jwt_data
 
-    def encode_payload(self): # TODO
+    def encode_payload(self):
         self.write_string(self.jwt_data)
