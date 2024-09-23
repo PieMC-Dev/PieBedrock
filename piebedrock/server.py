b705d7237b8f190819cc00acd9fff34e5742f413
Andoni <andiricum2@gmail.com>
2024-09-23 14:10:34 +0200

    Update server.py

--- piebedrock/server.py
+++ piebedrock/server.py
@@ -1,14 +1,18 @@
 from pieraknet.server import Server as RakNetServer
 
 class BedrockServer:
-    #init
     def __init__(self):
         self.protocol_version = 712
 
     def main(self):
         from piebedrock.interface import GameInterface
+        server = BedrockServer()
         interface = GameInterface()
-        server = RakNetServer(logginglevel = "INFO")
+        server = RakNetServer()
+        server.logger.setLevel("INFO")
+        server.game="MCPE"
+        server.name="My Minecraft Server"
+        server.modt="PieBedrock Server"
         server.interface = interface
 
         server.start()
